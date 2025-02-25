173e0ab2991ad7d0b25fe5d2d2b1955fd1d3dcec
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:37:39 -0800

    Book inventory

---
+++ Models/Entity/Inventory.cs
@@ -0,0 +1,10 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int StockQuantity { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
