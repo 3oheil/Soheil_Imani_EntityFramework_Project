fb42b6c6a2397e025b29e5c460d1972e2da4486d
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-25 08:37:19 -0800

    Map enitity by Fluent api

---
+++ Mapping/BookConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Soheil_Imani_EntityFramework_Project.Models.Entity;
+
+namespace Soheil_Imani_EntityFramework_Project.Mapping
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b=>b.Title).IsRequired().HasMaxLength(50);
+            builder.Property(b => b.ISBN).IsRequired().HasMaxLength(50);
+            builder.Property(b => b.Price).IsRequired().HasColumnType("decimal(18, 2)");
+            builder.Property(b => b.StockQuantity).IsRequired().HasMaxLength(20);
+            builder.Property(b => b.CategoryId).IsRequired();
+            builder.Property(b => b.PublisherYear).IsRequired();
+            builder.Property(b => b.PublisherId).IsRequired();
+            builder.Property(b => b.Language).IsRequired().HasMaxLength(50);
+            builder.Property(b => b.Description).HasColumnType("Text").HasMaxLength(500);
+
+            builder.HasOne(b=>b.Author).WithMany(a=>a.Books).HasForeignKey(b=>b.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(c=>c.Category).WithMany(a=>a.Books).HasForeignKey(b=>b.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(b => b.Publisher).WithMany(p=>p.Books).HasForeignKey(b=>b.PublisherId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Mapping/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Soheil_Imani_EntityFramework_Project.Models.Entity;
+
+namespace Soheil_Imani_EntityFramework_Project.Mapping
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u=>u.Id);
+            builder.Property(u=>u.FullName).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.PhoneNumber).IsRequired().HasMaxLength(15);
+            builder.Property(u => u.Address).IsRequired().HasMaxLength(55);
+            builder.Property(u => u.Email).IsRequired().HasMaxLength(25);
+            builder.Property(u => u.PasswordHash).IsRequired();
+            builder.Property(u => u.Role).IsRequired().HasMaxLength(15);
+
+            //builder.HasMany(u=>u.Orders).WithOne(o=)
+
+
+        }
+    }
+}
