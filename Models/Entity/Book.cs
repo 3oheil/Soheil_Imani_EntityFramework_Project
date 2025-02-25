618d4e29b173a1223b8b7afdbfe03ffc6afcff4b
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:31:21 -0800

    Book class properties

---
+++ Models/Entity/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Athor { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public int CategoryId { get; set; }
+        public int PublisherId { get; set; }
+        public int PublisherYear { get; set; }
+        public int PageCount { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public string CoverImageUrl { get; set; }
+        public double Rating { get; set; }
+        public ICollection<Review> Review { get; set; } = new List<Review>();
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
