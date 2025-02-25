27af7434db1e01273d6da5b90692d0922b2956b4
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:34:41 -0800

    Shopping cart properties for the user

---
+++ Models/Entity/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public decimal TotalPrice { get; set; }
+    }
+}
