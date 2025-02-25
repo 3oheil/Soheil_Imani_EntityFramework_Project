3a4515c34b68a7bdcd47579e22a0e5c1e7abc233
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-25 08:35:31 -0800

    Autor class Mapping

---
+++ Mapping/AuthorConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Soheil_Imani_EntityFramework_Project.Models.Entity;
+
+namespace Soheil_Imani_EntityFramework_Project.Mapping
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a=>a.FullName).IsRequired().HasMaxLength(128);
+            builder.Property(a=>a.Biography).HasMaxLength(1000).HasColumnType("text");
+
+
+        }
+    }
+}
