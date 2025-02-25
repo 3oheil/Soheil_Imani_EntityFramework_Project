61cfac57dc3a964ff4a176e97b0e678014b558d5
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:32:00 -0800

    Order class properties

---
+++ Models/Entity/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string OrderStatus { get; set; }
+        public string ShippingAddress { get; set; }
+        public ICollection<OrderItem> OrderItem { get; set; } = new List<OrderItem>();
+
+    }
+}
