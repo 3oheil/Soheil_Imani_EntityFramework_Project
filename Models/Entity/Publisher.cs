d51c0116181a9d1510fdaf0e3988cf7c632211b4
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:32:14 -0800

    Publisher class properties

---
+++ Models/Entity/Publisher.cs
@@ -0,0 +1,11 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Wsbsite { get; set; }
+        public string Country{ get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
