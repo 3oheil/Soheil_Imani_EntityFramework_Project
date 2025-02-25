49b58b0bb3b4898239858404628ce9d2a8bbb5cc
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:36:36 -0800

    Book categories

---
+++ Models/Entity/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
