ad6c20ace9d7fd27eee67b4ffc289c90829954c8
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:31:41 -0800

    User class properties

---
+++ Models/Entity/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public ICollection<Review> Review { get; set; } = new List<Review>();
+    }
+}
