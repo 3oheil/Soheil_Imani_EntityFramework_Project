78a5db91afcffc58a6204793054fdea2edfc50a6
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:32:47 -0800

    Notification class properties for the user

---
+++ Models/Entity/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
