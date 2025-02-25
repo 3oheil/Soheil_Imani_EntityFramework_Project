b66b033481652056fe1870a4343fac4c186fabd7
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:33:31 -0800

    User order payment class properties

---
+++ Models/Entity/Payment.cs
@@ -0,0 +1,12 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string PaymentMethod { get; set; }
+        public string PaymentStatus { get; set; }
+        public string TransationId { get; set; }
+        public DateTime PaymantDate { get; set; }
+    }
+}
