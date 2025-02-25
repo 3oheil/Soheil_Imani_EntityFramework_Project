fb1a912407e5e17283e21c9265b34a2969323441
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-02-24 20:31:04 -0800

    Author class properties

---
+++ Models/Entity/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Soheil_Imani_EntityFramework_Project.Models.Entity
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FullName{ get; set; }
+        public string Biography { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
